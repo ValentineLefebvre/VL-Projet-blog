2690a77b22388697004caa7e5924d17dbb940703
Valentine <100138762+ValentineLefebvre@users.noreply.github.com>
2022-10-16 23:14:06 +0200

    Delete src/php_partial directory

--- src/php_partial/get_script.php
+++
@@ -1,8 +0,0 @@
-<!-- select right script by checking url -->
-<?php if($uri === "/entrer.php") :?>
-    <!-- script here -->
-<?php elseif($uri === "/login") :?>
-    <!-- script here -->
-<?php elseif($uri === "/timeline") :?>
-    <script src="script/timeline_script.js?<?php echo time(); ?>"></script>
-<?php endif; ?>
